5a566062674295d9fa15aae4a1abf9677fb945ec
泽 木 <2448974318@qq.com>
2020-01-17 19:57:51 +0800

    feat：新增一个springboot项目

--- SpringBootDemo/src/main/java/com/lzh/learn/SpringBootDemo/SpringBootDemoApplication.java
+++ SpringBootDemo/src/main/java/com/lzh/learn/SpringBootDemo/SpringBootDemoApplication.java
@@ -11,7 +11,7 @@ public class SpringBootDemoApplication {
 
 	@RequestMapping("")
 	String home() {
-		return "Hello World!";
+		return "Hello World!!!!";
 	}
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootDemoApplication.class, args);
